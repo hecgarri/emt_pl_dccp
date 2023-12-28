fb6b175a0931c48725b61d49950b9e222bf0186f
Hector Garrido <hector.garrido@direcciondecompras.local>
2023-12-28 11:18:50 -0300

    Update

--- 20231212_calculos_emt_pl.R
+++ 20231212_calculos_emt_pl.R
@@ -352,6 +352,7 @@ SolicitudCotizacion <- sqlQuery(con2, "SELECT TOP (1000) SC.[Id]
       WHERE YEAR(FechaPublicacion) = 2023")
 
 
+
 # Plan de análisis: 
 # 1. cruzar las tablas gblEnterpriseAddress con prcPOHeader, a través de los campos eadCode y porShipAddress
 # 2. Verificar en qué proporción coinciden los campos porShipAddress y porInVoiceAddress
@@ -363,11 +364,70 @@ SolicitudCotizacion <- sqlQuery(con2, "SELECT TOP (1000) SC.[Id]
 #  solicitudes de cotización del módulo de compra ágil y la informacción contenida
 #  en licitaciones
 
+start <- Sys.time()
+data <- sqlQuery(con2, "
+                 SELECT DISTINCT
+                  porCode
+                  ,porDescription
+                  ,PI.poiName
+                  ,PI.poiDescription
+                  ,[porBuyerOrganization]
+                  ,E.entCode
+                  ,O.orgLegalName [Nombre Unidad de Compra]
+                  ,E.entName [Nombre Organismo Comprador]
+                  ,O2.orgLegalName [Nombre Proveedor (sucursal)]
+                  , E2.entName [Nombre Proveedor (matriz)]
+                  , EA.eadName [Nombre dirección Unidad de Compra]
+                  , EA.eadAddress [Dirección \n Unidad de Compra]
+                  , C.citName [Región \n Unidad de Compra]
+                  , D.disName [Comuna \n Unidad de Compra]
+                  , (CASE WHEN C.citCode = C2.citCode THEN 1 ELSE 0 END) [Proveedor Local]
+                  , EA2.eadName [Nombre dirección sucursal]
+                  , EA2.eadAddress [Dirección \n Sucursal]
+                  , C2.citName [Región \n Sucursal]
+                  , D2.disName [Comuna \n Sucursal]
+                  , ST.pstDescription
+                  ,[porShipmentType] -- Tipo de Despacho
+                  ,[porInvoiceAddress] -- Dirección de facturación 
+                  ,[porShipAddress] -- Dirección de despacho
+                  ,[porShipInstructions]
+                  ,[porHandlingInstructions]
+                  ,[porSpecialInstructions]
+                  ,[porDeliveryInstructions]
+                 FROM(
+                  SELECT TOP 1000 [porID]
+                  FROM [DCCPProcurement].[dbo].[prcPOHeader] PO
+                  WHERE porIsIntegrated = 3
+                      AND (YEAR([porSendDate]) = 2023 AND MONTH([porSendDate]) = 11) 
+                      AND ([porBuyerStatus] IN (4, 5, 6, 7, 12)) 
+                  ORDER BY NEWID() -- Asegura la selección aleatoria
+                  ) AS Subquery
+        				 INNER JOIN [DCCPProcurement].[dbo].[prcPOHeader] PO ON Subquery.[porID] = PO.[porID]
+    		         INNER JOIN [DCCPPlatform].[dbo].[gblOrganization] O ON PO.porBuyerOrganization = O.orgCode -- Identifica unidad de compra
+                 INNER JOIN [DCCPPlatform].[dbo].[gblEnterprise] E ON O.orgEnterprise = E.entCode      -- Identifica organismo comprador           
+                 INNER JOIN [DCCPPlatform].[dbo].[gblOrganizationAddress] OA ON O.orgCode = OA.oraOrganization
+                 INNER JOIN [DCCPPlatform].[dbo].[gblEnterpriseAddress] EA ON OA.oraAddress = EA.eadCode
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblCity] C ON EA.eadCity = C.citCode
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblDistrict] D ON EA.eadDistrict = D.disCode
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblOrganization] O2 ON PO.porSellerOrganization = O2.orgCode -- Identifica sucursal prov.
+                 INNER JOIN [DCCPPlatform].[dbo].[gblEnterprise] E2 ON O2.orgEnterprise = E2.entCode -- Identifica Proveedor
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblOrganizationAddress] OA2 ON O2.orgCode = OA2.oraOrganization 
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblEnterpriseAddress] EA2 ON OA2.oraAddress = EA2.eadCode
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblCity] C2 ON EA2.eadCity = C2.citCode
+        				 INNER JOIN [DCCPPlatform].[dbo].[gblDistrict] D2 ON EA2.eadDistrict = D2.disCode
+                 INNER JOIN [DCCPPlatform].[dbo].[gblAddressType] AT ON OA.oraType = AT.atyCode
+                 INNER JOIN [DCCPProcurement].[dbo].[prcPOShipmentType] ST ON PO.porShipmentType = ST.pstCode 
+        				 INNER JOIN [DCCPProcurement].[dbo].[prcPOItem] PI ON PO.[porID] = PI.poiID
+
+                 ")
+end <- Sys.time()
+difftime(end, start, units = "mins")
 
 
-
-
-
+data %>% 
+  group_by(poiDescription) %>% 
+  summarise(n_ = n()) %>%  
+  order_by(desc(n_))
 
 
 
