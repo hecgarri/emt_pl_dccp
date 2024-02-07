2762c041912728eaedf7152a57d3b037533e9d21
Hector Garrido <hector.garrido@direcciondecompras.local>
2024-02-07 09:13:31 -0300

    update

--- 20240123 descarga datos.R
+++ 20240123 descarga datos.R
@@ -33,7 +33,8 @@ packages = c("tidyverse" #Conjunto integral de paquetes para manipular y analiza
              , "shiny"
              , "ggplot2"
              , "DT"
-             , "shinyjs")
+             , "shinyjs"
+             , "sqldf")
 
 
 load_pkg(packages)
@@ -73,7 +74,8 @@ consultar_y_guardar <- function(x,y, window = -11
     con3 = RODBC::odbcConnect("dw", uid = "datawarehouse", pwd = "datawarehouse") #Datawarehouse
   
   if (length(years) == 0 | !tipoConsulta %in% c('cotizaciones compra ágil'
-                                                 , 'órdenes de compra')) {
+                                                 , 'órdenes de compra'
+                                                ,'usuarios proveedores')) {
     mensaje <- "Parámetros inválidos. Asegúrate de proporcionar años y/o tipo de consulta válidos."
     return(mensaje)
   }
@@ -85,7 +87,7 @@ consultar_y_guardar <- function(x,y, window = -11
   # porque al usar <- en lugar de = arroja un error. OJO 
   
   ejecutarConsulta <- switch (tipoConsulta,
-    'cotizaciones compra ágil' = function(x, y, window ) {
+    'cotizaciones compra ágil' = function(x = x, y = y, window = window) {
       sqlQuery(con2, sprintf(
       "
 		  DECLARE @MONTH AS INT;
@@ -97,9 +99,11 @@ consultar_y_guardar <- function(x,y, window = -11
       DECLARE @CURRENTMONTH datetime = datetimefromparts(@YEAR, @MONTH, 1,0,0,0,0);
       DECLARE @startDate datetime = dateadd(month,%s, @currentMonth)
       , @endDate datetime = dateadd(month, 1, @currentMonth);
-      
-      WITH TABLA AS (SELECT 
+		---------------------------------------------------------------------------------------------------------  
+     WITH TABLA AS (SELECT 
 	     SC.CodigoSolicitudCotizacion
+		  ,YEAR(SC.FechaPublicacion) [year]
+		  ,MONTH(SC.FechaPublicacion) [month]
 		  ,SC.Descripcion
 		  ,SC.FechaPublicacion
 		  ,CP.CodigoProducto
@@ -118,7 +122,11 @@ consultar_y_guardar <- function(x,y, window = -11
 		  ,CO.EsProveedorSeleccionado
 		  ,CO.EsOfertaParcial
 		  ,O1.orgActivity [Actividad Proveedor]
-		  ,O2.orgLegalName [Razón Social Comprador]
+		  ,O2.orgLegalName [Razón Social Unidad de Compra]
+		  ,E2.entName [Razón Social Comprador]
+		  ,O2.orgActivity
+		  ,O2.orgParentOrganization
+		  ,O2.orgTaxID
 		  ,SC.CodigoRegion [Región de Despacho]
 		,(CASE  
 		WHEN SII.TRAMOS_13 IS NULL THEN 'Sin categoría' -- 
@@ -145,6 +153,7 @@ consultar_y_guardar <- function(x,y, window = -11
 				WHEN SII.[DESCRIPCION_REGION] IS NULL THEN 'Sin categoría'
 				ELSE 6 
 				END) AS id_regionProv
+		---------------------------------------------------------------------------------------------------------
 		FROM [DCCPCotizacion].[dbo].[SolicitudCotizacion] as SC --TABLESAMPLE (1000 ROWS) REPEATABLE (123)
 	  LEFT JOIN  [DCCPCotizacion].[dbo].[Cotizacion] as co ON co.SolicitudCotizacionId=SC.id and CO.ESTADOID = 2
 	  INNER JOIN [DCCPCotizacion].[dbo].[CotizacionProducto] as CP ON CO.Id = CP.CotizacionId
@@ -152,10 +161,11 @@ consultar_y_guardar <- function(x,y, window = -11
 	  INNER JOIN [DCCPPlatform].[dbo].[gblOrganization] as O1 ON CO.CodigoSucursalEmpresa COLLATE Modern_Spanish_CI_AI = O1.orgCode COLLATE Modern_Spanish_CI_AI
 	  INNER JOIN [DCCPPlatform].[dbo].[gblEnterprise] as E ON CO.CodigoEmpresa = E.entCode COLLATE Modern_Spanish_CI_AI
 	  INNER JOIN [DCCPPlatform].[dbo].[gblOrganization] as O2 ON SC.CodigoOrganismo  COLLATE Modern_Spanish_CI_AI = O2.orgCode COLLATE Modern_Spanish_CI_AI
+	  INNER JOIN [DCCPPlatform].[dbo].[gblEnterprise] as E2 ON O2.orgEnterprise = E2.entCode
 	  INNER JOIN [Estudios].[dbo].[sii_atrib_2021] as SII ON CAST(LEFT(LOWER(REPLACE(REPLACE(O1.orgTaxID,'.',''),'-',''))
         			,LEN(LOWER(REPLACE(REPLACE(O1.orgTaxID,'.',''),'-','')))-1) as VARCHAR(50)) = CAST(SII.RUT AS VARCHAR(50))  
 	  INNER JOIN [Estudios].[dbo].[THTamanoProveedor] as TP ON TP.entcode = E.entCode AND TP.[AñoTributario]=2022 
-	  ---------------------------------------------------
+ 		---------------------------------------------------------------------------------------------------------
 	  WHERE (SC.FechaPublicacion < @endDate) AND
           (SC.FechaPublicacion >= @startDate)
 	  ) 
@@ -170,8 +180,8 @@ consultar_y_guardar <- function(x,y, window = -11
   ",x,y, window)
               ) 
               },
-  'órdenes de compra' = function(x,y,window){
-    sqlQuery(con3, sprintf(
+  'órdenes de compra' = function(x = x, y = y, window = window) {
+    sqlQuery(con3,sprintf( 
       "
     DECLARE @MONTH AS INT;
     DECLARE @YEAR AS INT;
@@ -244,9 +254,63 @@ consultar_y_guardar <- function(x,y, window = -11
   WHERE (T.Date < @endDate) 
   AND (T.Date >= @startDate) 
   AND OC.IDEstadoOC >= 5
-      ",x,y, window)
-    )
-  }
+      ",x,y, window))
+  
+  }, 'usuarios proveedores' = function(x = x, y = y, window = window) {
+    query <- paste("DECLARE @MONTH AS INT;
+      DECLARE @YEAR AS INT;
+                  
+      SET @MONTH = ",x,";
+      SET @YEAR = ",y,";
+                  
+      DECLARE @CURRENTMONTH datetime = datetimefromparts(@YEAR, @MONTH, 1,0,0,0,0);
+      DECLARE @startDate datetime = dateadd(month,",window,", @currentMonth)
+      , @endDate datetime = dateadd(month, 1, @currentMonth);
+
+                SELECT DISTINCT
+                 CAST(
+					CASE 
+						WHEN LEN(LOWER(REPLACE(REPLACE(O.orgTaxID,'.',''),'-',''))) > 1 
+						THEN LEFT(LOWER(REPLACE(REPLACE(O.orgTaxID,'.',''),'-',''))
+						, LEN(LOWER(REPLACE(REPLACE(O.orgTaxID,'.',''),'-',''))) - 1)
+						ELSE 'Sin rut: ' + O.orgTaxID 
+					END as VARCHAR(50)) [Rut Proveedor]
+                ,O.orgEnterprise [EntCode]
+                ,UPPER(E.entname) [Razon social]
+                , U.usrLastLogin
+			    , MONTH(U.usrLastLogin) [Mes]
+                , YEAR(U.usrLastLogin) [Anio]
+				,U.usrEmail
+				,U.usrFirstName+' '+U.usrLastName [Nombre Usuario]
+				,U.usrTaxID
+				,U.usrMobile
+				,U.usrPhone
+				,U.usrPosition
+				, @MONTH [Mes Central]
+				, @YEAR [Anio Central]
+				, @startDate [Comienzo]
+				, @endDate [Final]
+                
+                FROM  [DCCPPlatform].[dbo].[gblSecUserRole] as UR 
+                INNER JOIN  [DCCPPlatform].[dbo].gblOrganization as O ON UR.uroOrganization      = O.orgCode
+                INNER JOIN  [DCCPPlatform].[dbo].GblUser as U ON UR.uroUser              = U.usrCode
+                LEFT JOIN   [DCCPPlatform].[dbo].gblEnterprise  as E  ON O.orgEnterprise         = E.entcode
+                WHERE  U.usrIsActive       = 1
+                AND O.orgIsActive   = 1
+                AND E.entIsActive   = 1
+                and o.orgistest = 0
+                AND (U.usrEmail NOT LIKE '%ontraloria.cl' OR (U.usrEmail LIKE '%ontraloria.cl' AND E.entCode = '7231'))  -- 7231 Codigo de la Contraloria
+                AND (U.usrEmail NOT LIKE '%ontroloria.cl' OR (U.usrEmail LIKE '%ontroloria.cl' AND E.entCode = '7231'))
+                AND E.entName NOT IN ('MERCADOPUBLICOTEST','MPCOMPRADORTEST_SKY','MPCOMPRADORTEST_SKY2','DCCP-OPERACIONES-PRUEBA COMPRADOR') -- Usuarios de Prueba
+                AND U.usrPosition != ''             -- No consideramos contactos sin usrPosition
+                AND U.usrEmail  != ''        -- No consideramos contactos sin mail
+                --AND YEAR(U.usrLastLogin) = @YEAR
+                AND U.usrLastLogin <= @endDate AND U.usrLastLogin >= @startDate
+                AND O.orgClass = 1
+                AND o.orgtaxid not in ('0-0','0.000.000-0','1-9','A.t21-125','yyyyyyyyyy')")
+    
+    sqlQuery(con2, query)
+    }
   )
   
   descargar_guardar <- function(x, y, window) {
@@ -313,7 +377,7 @@ consultar_y_guardar <- function(x,y, window = -11
   }
   
   # Guarda el objeto data en un archivo con nombre diferente según el tipo de consulta
-  saveRDS(total, file = paste0(gsub("-", "", today()), gsub(" ", "_", tipoConsulta), " en algún procedimiento de compra ", y, ".rds"))
+  saveRDS(total, file = paste0(gsub("-", "", today()),gsub(" ", "_", tipoConsulta), ".rds"))
 } else {
   total <- readRDS(file = file.path(wd_path, datasets[grep(tipoConsulta, datasets$files)[1],c("files")]))  
   }
@@ -325,21 +389,31 @@ consultar_y_guardar <- function(x,y, window = -11
 
 years <- c(2023, 2024)
 
-datos <-  consultar_y_guardar(wd_path = data_path
-                                 , x = 12
-                                 , y = years
-                                 , tipoConsulta = "cotizaciones compra ágil"
-                                 , depurar = FALSE
-                                 , updated = TRUE)
+datos <- consultar_y_guardar(wd_path = data_path
+                             , x = 12
+                             , y = years
+                             , tipoConsulta = "cotizaciones compra ágil"
+                             , depurar = FALSE
+                             , updated = TRUE)
 
-datos_oc <-  consultar_y_guardar(wd_path = data_path
+datos_oc <- consultar_y_guardar(wd_path = data_path
                               , x = 12
                               , y = years
                               , tipoConsulta = "órdenes de compra"
                               , depurar = FALSE
-                              , updated = FALSE)
+                              , updated = TRUE)
 
+# datos_usuarios <- consultar_y_guardar(x = 12
+#                                       ,y = years
+#                                       , window = -2
+#                                       ,wd_path = data_path
+#                                       ,tipoConsulta = "usuarios proveedores"
+#                                       ,depurar = FALSE
+#                                       ,updated = TRUE)
 
+file <- "C:/o/OneDrive - DCCP/Escritorio/Proyectos/Preferencias EMT y Compras Regionales/emt_pl_dccp/datos/20240124usuarios_proveedores.rds"
+
+usuarios <- readRDS(file)
 
 regiones <- tibble(
   `Región de Despacho` = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
@@ -349,27 +423,71 @@ regiones <- tibble(
                     ,'Los Lagos','Aysén','Magallanes'
                     ,'Metropolitana','Los Ríos','Arica y Parinacota','Ñuble'))
 
-datos <- setDT(datos) %>% 
-  inner_join(regiones, by = c("Región de Despacho"))
+datos <- setDT(datos) %>%
+  inner_join(regiones, by = c("Región de Despacho")) %>%
+  inner_join(regiones, join_by(id_regionProv == `Región de Despacho`))
 
 
-datos_rubros <- datos %>% 
-  group_by(level1) %>% 
+datos_rubros <- datos %>%
+  group_by(year, level1) %>%
   summarise(ofertas = n_distinct(CodigoCotizacion)
             ,solicitudes = n_distinct(CodigoSolicitudCotizacion)
             ,proveedores = n_distinct(entCode))
 
 
-datos_rureg <- datos %>% 
-  group_by(NombreRegion, level1) %>% 
+datos_rureg <- datos %>%
+  group_by(NombreRegion.x, level1) %>%
   summarise(ofertas = n_distinct(CodigoCotizacion)
             ,solicitudes = n_distinct(CodigoSolicitudCotizacion)
             ,proveedores = n_distinct(entCode))
 
-datos_ruregMonth <- datos %>% 
+datos_ruregMonth <- datos %>%
   mutate(FechaPublicacion = as.Date(FechaPublicacion)
-         ,month = lubridate::month(FechaPublicacion)) %>% 
-  group_by(month,NombreRegion, level1) %>% 
+         ,month = lubridate::month(FechaPublicacion)) %>%
+  group_by(month,NombreRegion.x, level1) %>%
   summarise(ofertas = n_distinct(CodigoCotizacion)
             ,solicitudes = n_distinct(CodigoSolicitudCotizacion)
             ,proveedores = n_distinct(entCode))
+
+
+datos_ruregInst <- datos %>%
+  group_by(`Razón Social Comprador`,NombreRegion.x, level1) %>%
+  summarise(ofertas = n_distinct(CodigoCotizacion)
+             ,solicitudes = n_distinct(CodigoSolicitudCotizacion)
+             ,proveedores = n_distinct(entCode))
+
+
+usuarios_matched <- sqldf("
+                          select distinct
+                          datos.[Razón Social Comprador]
+                          ,datos.[Razón Social Unidad de Compra]
+                          ,datos.[NombreRegion.x]
+                          ,datos.[Razón Social Proveedor]
+                          ,datos.level1
+                          ,datos.level2
+                          ,datos.level3
+                          ,datos.productoname
+                          ,datos.[Región de Despacho]
+                          ,usuarios.[Rut Proveedor]
+                          ,usuarios.[Mes]
+                          ,usuarios.[Anio]
+                          ,usuarios.[usrEmail]
+                          ,usuarios.[Nombre Usuario]
+                          ,usuarios.[usrTaxID]
+                          ,usuarios.[usrPhone]
+                          ,usuarios.[usrPosition]
+                          from datos 
+                          inner join usuarios on datos.entCode = usuarios.EntCode
+                          ") 
+
+
+# Identificar aquel proveedor que se adjudica el procedimiento además de la cantidad solicitudes y ofertas
+# , el monto del mecanismo de compra
+# 
+
+# Denuncias y reclamos según proveedor
+# Cantidad de 
+
+
+# Calcular el porcentaje que representan aquellos rubros en que la compra a proveedores no locales es superior
+#  a los locales con respecto al total del mecanismo
